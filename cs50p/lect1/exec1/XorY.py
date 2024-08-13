12479fb8453855eb9c59b92d3b948e4c431a4518
mrfloki16 <mr_floki@hotmail.com>
2024-08-13 13:59:17 -0400

    adding exerc1

---
+++ cs50p/lect1/exec1/XorY.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+x = int(input("x = "))
+y = int(input("y = "))
+
+if x != y:
+  print("x is not equal to y")
+else:
+  print("x is equal to y")
---
+++ cs50p/lect1/exec1/match_case.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+name = input("What's your name? ")
+
+match name: 
+  case "Harry" | "Hermione" | "Ron":
+    print("Gryffindor")
+  case "Draco":
+    print("Slytherin")
+  case _:
+    print("Who?")
