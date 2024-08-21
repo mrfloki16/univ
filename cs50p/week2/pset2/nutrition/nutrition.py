996d17a45fa8c58133a48b29ec9d46f5f3843fe5
mrfloki16 <mr_floki@hotmail.com>
2024-08-21 09:41:35 -0400

    addind nutrition pset2

--- cs50p/week2/pset2/nutrition/nutrition.py
+++ cs50p/week2/pset2/nutrition/nutrition.py
@@ -1,3 +1,30 @@
 #!/bin/python3
 
+def main():
+  item = input("Item : ")
+  cal = item.lower()
+  if cal in nutrition_dict:
+    print(f"Calories : {nutrition_dict[cal]}")
 
+nutrition_dict = {
+  "apple" : 130,
+  "avocado" : 50,
+  "banana" : 110,
+  "cantaloupe" : 50,
+  "grapefruit" : 60,
+  "grapes" : 90,
+  "honeydew melon" : 50,
+  "kiwifruit" : 90,
+  "lemon" : 15,
+  "lime" : 20,
+  "nectarine" : 60,
+  "pear" : 100,
+  "pineapple" : 50,
+  "plums" : 70,
+  "strawberries" : 50,
+  "sweet cherries" : 100,
+  "tangerine" : 50,
+  "watermelon" : 80,
+}
+
+main()
